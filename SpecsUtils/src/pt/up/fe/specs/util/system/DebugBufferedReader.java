4fd3113edad84f956112b06d5ecde654922a1949
JoaoBispo <joaobispo@gmail.com>
2017-08-01 17:21:05 +0100

    [SpecsUtils] Adds DebugBufferedReader

---
+++ SpecsUtils/src/pt/up/fe/specs/util/system/DebugBufferedReader.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2017 SPeCS.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. under the License.
+ */
+
+package pt.up.fe.specs.util.system;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class DebugBufferedReader extends BufferedReader {
+
+    public DebugBufferedReader(BufferedReader reader) {
+        super(reader);
+    }
+
+    @Override
+    public int read() throws IOException {
+        int readInt = super.read();
+        System.out.println("DebugReader: read() -> " + readInt + " (" + ((char) readInt) + ")");
+        return readInt;
+    }
+
+    @Override
+    public int read(char[] cbuf, int off, int len) throws IOException {
+
+        int readInt = super.read(cbuf, off, len);
+        System.out.println("DebugReader: read(char[], int, int) -> " + readInt + " (" + ((char) readInt) + ")");
+        return readInt;
+    }
+
+    @Override
+    public String readLine() throws IOException {
+        String line = super.readLine();
+        System.out.println("DebugReader: readLine() -> " + line);
+        return line;
+    }
+
+}
