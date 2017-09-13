96c8abbe55b0daae0e81910df7f145bae5c16736
Tiago Carvalho <tiagodrcarvalho@gmail.com>
2017-09-13 10:14:58 +0200

    [tdrcLibrary] Adds distance calculator to Tuple class

--- tdrcLibrary/src/tdrc/tuple/Tuple.java
+++ tdrcLibrary/src/tdrc/tuple/Tuple.java
@@ -17,10 +17,15 @@ import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiFunction;
 
-public class Tuple<T> extends AbstractList<T> {
+import org.apache.commons.lang3.builder.CompareToBuilder;
+
+public class Tuple<T> extends AbstractList<T> implements Comparable<Tuple<T>> {
 
     List<T> tuple;
+    BiFunction<Tuple<T>, Tuple<T>, Integer> comparator;
+    private BiFunction<Tuple<T>, Tuple<T>, Double> distanceCalculator;
 
     public static <T> Tuple<T> newInstance() {
         return new Tuple<>();
@@ -37,6 +42,8 @@ public class Tuple<T> extends AbstractList<T> {
 
     private Tuple(List<T> elements) {
         tuple = new ArrayList<>(elements);
+        this.comparator = Tuple::defaultComparator;
+        this.distanceCalculator = Tuple::defaultDistanceCalculator;
     }
 
     private Tuple() {
@@ -85,5 +92,103 @@ public class Tuple<T> extends AbstractList<T> {
             }
         }
         return true;
+    }
+
+    @Override
+    public int compareTo(Tuple<T> o) {
+        return comparator.apply(this, o);
+    }
+
+    /*@SuppressWarnings({ "rawtypes", "unchecked" })
+    public int compareTo(final Tuple o) {
+        
+        final int tLen = this.valueArray.length;
+        final Object[] oValues = o.valueArray;
+        final int oLen = oValues.length;
+        
+        for (int i = 0; i < tLen && i < oLen; i++) {
+            
+            final Comparable tElement = (Comparable)this.valueArray[i];
+            final Comparable oElement = (Comparable)oValues[i];
+            
+            final int comparison = tElement.compareTo(oElement);
+            if (comparison != 0) {
+                return comparison;
+            }
+            
+        }
+        
+        return (Integer.valueOf(tLen)).compareTo(Integer.valueOf(oLen));
+        
+    }*/
+    public static <T> Integer defaultComparator(Tuple<T> tuple, Tuple<T> tuple2) {
+        CompareToBuilder compareToBuilder = new CompareToBuilder();
+        final int tLen = tuple.size();
+        final int oLen = tuple2.size();
+        for (int i = 0; i < tLen && i < oLen; i++) {
+            compareToBuilder.append(tuple.get(i), tuple2.get(i));
+        }
+
+        return compareToBuilder.toComparison();
+
+        // final int tLen = tuple.size();
+        // final int oLen = tuple2.size();
+        //
+        // for (int i = 0; i < tLen && i < oLen; i++) {
+        //
+        // final Comparable<T> tElement = (Comparable) tuple.get(i);
+        // // final Comparable<T> oElement = (Comparable) tuple2.get(i);
+        //
+        // final int comparison = tElement.compareTo(tuple2.get(i));
+        // if (comparison != 0) {
+        // return comparison;
+        // }
+        // }
+        // return tLen - oLen;
+    }
+
+    public double getDistance(Tuple<T> otherTuple) {
+        return distanceCalculator.apply(this, otherTuple);
+    }
+
+    public static <T> Double defaultDistanceCalculator(Tuple<T> tuple, Tuple<T> tuple2) {
+
+        final int tLen = tuple.size();
+        final int oLen = tuple2.size();
+        double sum = 0;
+        for (int i = 0; i < tLen && i < oLen; i++) {
+            T t = tuple.get(i);
+            T t2 = tuple2.get(i);
+            if (!(t instanceof Number && t2 instanceof Number)) {
+                throw new RuntimeException("Cannot calculate distance if the elements of the tuple are not numbers");
+            }
+            double tD = ((Number) t).doubleValue() - ((Number) t2).doubleValue();
+            sum += tD * tD;
+        }
+
+        return Math.sqrt(sum);
+
+        // final int tLen = tuple.size();
+        // final int oLen = tuple2.size();
+        //
+        // for (int i = 0; i < tLen && i < oLen; i++) {
+        //
+        // final Comparable<T> tElement = (Comparable) tuple.get(i);
+        // // final Comparable<T> oElement = (Comparable) tuple2.get(i);
+        //
+        // final int comparison = tElement.compareTo(tuple2.get(i));
+        // if (comparison != 0) {
+        // return comparison;
+        // }
+        // }
+        // return tLen - oLen;
+    }
+
+    public BiFunction<Tuple<T>, Tuple<T>, Double> getDistanceCalculator() {
+        return distanceCalculator;
+    }
+
+    public void setDistanceCalculator(BiFunction<Tuple<T>, Tuple<T>, Double> distanceCalculator) {
+        this.distanceCalculator = distanceCalculator;
     }
 }
