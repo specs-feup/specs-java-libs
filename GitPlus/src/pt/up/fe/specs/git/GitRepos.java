6ced6014c1039ffabc6071212333bf072a04a0a8
JoaoBispo <joaobispo@gmail.com>
2017-12-20 03:53:33 +0000

    [GitPlus] Using global object to store information about git repositories in the system

--- GitPlus/src/pt/up/fe/specs/git/GitRepos.java
+++ GitPlus/src/pt/up/fe/specs/git/GitRepos.java
@@ -14,16 +14,20 @@
 package pt.up.fe.specs.git;
 
 import java.io.File;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class GitRepos {
 
     // Maps repo names to the folder where they are in the system
-    private final Map<String, File> repos;
+    // One instance per JVM
+    private static final Map<String, File> REPOS = new ConcurrentHashMap<>();
+
+    // Maps repo names to the folder where they are in the system
+    // private final Map<String, File> repos;
 
     public GitRepos() {
-        this.repos = new HashMap<>();
+        // this.repos = new HashMap<>();
     }
 
     public File getFolder(String repositoryPath) {
@@ -31,10 +35,12 @@ public class GitRepos {
         String repoName = SpecsGit.getRepoName(repositoryPath);
 
         // Check if it is already in the map
-        File repoFolder = repos.get(repoName);
+        // File repoFolder = repos.get(repoName);
+        File repoFolder = REPOS.get(repoName);
         if (repoFolder == null) {
             repoFolder = SpecsGit.parseRepositoryUrl(repositoryPath);
-            repos.put(repoName, repoFolder);
+            // repos.put(repoName, repoFolder);
+            REPOS.put(repoName, repoFolder);
         }
 
         return repoFolder;
