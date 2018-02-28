8e47a1b44e408c5adefed9f904d4d5a3263a37fb
JoaoBispo <joaobispo@gmail.com>
2018-02-28 02:25:21 +0000

    [SpecsUtils] Adds methods in SpecsSystem for dynamically loading classes

--- SpecsUtils/src/pt/up/fe/specs/util/SpecsSystem.java
+++ SpecsUtils/src/pt/up/fe/specs/util/SpecsSystem.java
@@ -20,13 +20,21 @@ import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryPoolMXBean;
 import java.lang.management.MemoryUsage;
 import java.lang.reflect.Field;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -614,7 +622,8 @@ public class SpecsSystem {
 
         String realArch = arch.endsWith("64")
                 || wow64Arch != null && wow64Arch.endsWith("64")
-                        ? "64" : "32";
+                        ? "64"
+                        : "32";
 
         if (realArch.equals("32")) {
             return false;
@@ -797,5 +806,71 @@ public class SpecsSystem {
         // pos = version.indexOf('.', pos + 1);
         return Double.parseDouble(version.substring(0, pos));
     }
+
+    /***** Methods for dynamically extending the classpath *****/
+    /***** Taken from https://stackoverflow.com/a/42052857/1189808 *****/
+    private static class SpclClassLoader extends URLClassLoader {
+        static {
+            ClassLoader.registerAsParallelCapable();
+        }
+
+        private final Set<Path> userLibPaths = new CopyOnWriteArraySet<>();
+
+        private SpclClassLoader() {
+            super(new URL[0]);
+        }
+
+        @Override
+        protected void addURL(URL url) {
+            super.addURL(url);
+        }
+
+        protected void addLibPath(String newpath) {
+            userLibPaths.add(Paths.get(newpath).toAbsolutePath());
+        }
+
+        @Override
+        protected String findLibrary(String libname) {
+            String nativeName = System.mapLibraryName(libname);
+            return userLibPaths.stream().map(tpath -> tpath.resolve(nativeName)).filter(Files::exists)
+                    .map(Path::toString).findFirst().orElse(super.findLibrary(libname));
+        }
+    }
+
+    private final static SpclClassLoader ucl = new SpclClassLoader();
+
+    /**
+     * Adds a jar file or directory to the classpath. From Utils4J.
+     *
+     * @param newpaths
+     *            JAR filename(s) or directory(s) to add
+     * @return URLClassLoader after newpaths added if newpaths != null
+     */
+    public static ClassLoader addToClasspath(String... newpaths) {
+        if (newpaths != null)
+            try {
+                for (String newpath : newpaths)
+                    if (newpath != null && !newpath.trim().isEmpty())
+                        ucl.addURL(Paths.get(newpath.trim()).toUri().toURL());
+            } catch (IllegalArgumentException | MalformedURLException e) {
+                RuntimeException re = new RuntimeException(e);
+                re.setStackTrace(e.getStackTrace());
+                throw re;
+            }
+        return ucl;
+    }
+
+    /**
+     * Adds to library path in ClassLoader returned by addToClassPath
+     *
+     * @param newpaths
+     *            Path(s) to directory(s) holding OS library files
+     */
+    public static void addToLibraryPath(String... newpaths) {
+        for (String newpath : Objects.requireNonNull(newpaths))
+            ucl.addLibPath(newpath);
+    }
+
+    /***** ENDS methods for dynamically extending the classpath *****/
 
 }
