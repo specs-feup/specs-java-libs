17e319a4f1fa59ed95bd9f6308fb134da7d7bd7c
JoaoBispo <joaobispo@gmail.com>
2019-03-13 01:03:11 +0000

    [SpecsUtils] Adds SpecsXml.getElementChildren()

--- SpecsUtils/src/pt/up/fe/specs/util/SpecsXml.java
+++ SpecsUtils/src/pt/up/fe/specs/util/SpecsXml.java
@@ -38,59 +38,59 @@ import org.xml.sax.SAXException;
 public class SpecsXml {
 
     public static NodeList getNodeList(File file) {
-	return getXmlRoot(file).getChildNodes();
+        return getXmlRoot(file).getChildNodes();
     }
 
     public static Document getXmlRoot(File file) {
-	try {
-	    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-	    DocumentBuilder dBuilder;
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder;
 
-	    dBuilder = dbFactory.newDocumentBuilder();
+            dBuilder = dbFactory.newDocumentBuilder();
 
-	    Document doc = dBuilder.parse(file);
+            Document doc = dBuilder.parse(file);
 
-	    // optional, but recommended
-	    // read this -
-	    // http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
-	    doc.getDocumentElement().normalize();
+            // optional, but recommended
+            // read this -
+            // http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+            doc.getDocumentElement().normalize();
 
-	    return doc;
-	} catch (ParserConfigurationException e) {
-	    SpecsLogs.msgWarn("Error message:\n", e);
-	} catch (SAXException e) {
-	    SpecsLogs.msgWarn("Error message:\n", e);
-	} catch (IOException e) {
-	    SpecsLogs.msgWarn("Error message:\n", e);
-	}
+            return doc;
+        } catch (ParserConfigurationException e) {
+            SpecsLogs.msgWarn("Error message:\n", e);
+        } catch (SAXException e) {
+            SpecsLogs.msgWarn("Error message:\n", e);
+        } catch (IOException e) {
+            SpecsLogs.msgWarn("Error message:\n", e);
+        }
 
-	return null;
+        return null;
     }
 
     public static Document getXmlRoot(String uri) {
-	try {
-	    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-	    DocumentBuilder dBuilder;
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder;
 
-	    dBuilder = dbFactory.newDocumentBuilder();
+            dBuilder = dbFactory.newDocumentBuilder();
 
-	    Document doc = dBuilder.parse(uri);
+            Document doc = dBuilder.parse(uri);
 
-	    // optional, but recommended
-	    // read this -
-	    // http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
-	    doc.getDocumentElement().normalize();
+            // optional, but recommended
+            // read this -
+            // http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+            doc.getDocumentElement().normalize();
 
-	    return doc;
-	} catch (ParserConfigurationException e) {
-	    SpecsLogs.msgWarn("Error message:\n", e);
-	} catch (SAXException e) {
-	    SpecsLogs.msgWarn("Error message:\n", e);
-	} catch (IOException e) {
-	    SpecsLogs.msgWarn("Error message:\n", e);
-	}
+            return doc;
+        } catch (ParserConfigurationException e) {
+            SpecsLogs.msgWarn("Error message:\n", e);
+        } catch (SAXException e) {
+            SpecsLogs.msgWarn("Error message:\n", e);
+        } catch (IOException e) {
+            SpecsLogs.msgWarn("Error message:\n", e);
+        }
 
-	return null;
+        return null;
     }
 
     /**
@@ -102,143 +102,143 @@ public class SpecsXml {
      * @return
      */
     public static String getAttribute(Document doc, String section, String attribute) {
-	NodeList nList = doc.getElementsByTagName(section);
+        NodeList nList = doc.getElementsByTagName(section);
 
-	return getAttribute(nList, section, attribute);
+        return getAttribute(nList, section, attribute);
     }
 
     public static String getAttribute(Element element, String section, String attribute) {
-	NodeList nList = element.getElementsByTagName(section);
+        NodeList nList = element.getElementsByTagName(section);
 
-	return getAttribute(nList, section, attribute);
+        return getAttribute(nList, section, attribute);
     }
 
     private static String getAttribute(NodeList nList, String section, String attribute) {
-	// NodeList nList = doc.getElementsByTagName(section);
-	/*
-		if (nList == null) {
-		    LoggingUtils.msgInfo("Could not find section '" + section + "'");
-		    return null;
-		}
-	 */
-	if (nList.getLength() == 0) {
-	    SpecsLogs.msgInfo("Could not find section '" + section + "'");
-	    return null;
-	}
+        // NodeList nList = doc.getElementsByTagName(section);
+        /*
+        	if (nList == null) {
+        	    LoggingUtils.msgInfo("Could not find section '" + section + "'");
+        	    return null;
+        	}
+         */
+        if (nList.getLength() == 0) {
+            SpecsLogs.msgInfo("Could not find section '" + section + "'");
+            return null;
+        }
 
-	if (nList.getLength() > 1) {
-	    SpecsLogs.msgInfo("Found more than one '" + section
-		    + "' section, returning the attribute of the first occcurence");
-	}
+        if (nList.getLength() > 1) {
+            SpecsLogs.msgInfo("Found more than one '" + section
+                    + "' section, returning the attribute of the first occcurence");
+        }
 
-	Node nNode = nList.item(0);
+        Node nNode = nList.item(0);
 
-	if (nNode.getNodeType() != Node.ELEMENT_NODE) {
-	    System.out.println("Node '" + section + "' is not an element");
-	    return null;
-	}
+        if (nNode.getNodeType() != Node.ELEMENT_NODE) {
+            System.out.println("Node '" + section + "' is not an element");
+            return null;
+        }
 
-	Element eElement = (Element) nNode;
+        Element eElement = (Element) nNode;
 
-	return eElement.getAttribute(attribute);
+        return eElement.getAttribute(attribute);
 
     }
 
     public static Element getSection(Element element, String section) {
-	NodeList nList = element.getElementsByTagName(section);
+        NodeList nList = element.getElementsByTagName(section);
 
-	if (nList.getLength() == 0) {
-	    SpecsLogs.msgInfo("Could not find section '" + section + "'");
-	    return null;
-	}
+        if (nList.getLength() == 0) {
+            SpecsLogs.msgInfo("Could not find section '" + section + "'");
+            return null;
+        }
 
-	if (nList.getLength() > 1) {
-	    SpecsLogs.msgInfo("Found more than one '" + section
-		    + "' section, returning the attribute of the first occcurence");
-	}
+        if (nList.getLength() > 1) {
+            SpecsLogs.msgInfo("Found more than one '" + section
+                    + "' section, returning the attribute of the first occcurence");
+        }
 
-	Node nNode = nList.item(0);
+        Node nNode = nList.item(0);
 
-	if (nNode.getNodeType() != Node.ELEMENT_NODE) {
-	    System.out.println("Node '" + section + "' is not an element");
-	    return null;
-	}
+        if (nNode.getNodeType() != Node.ELEMENT_NODE) {
+            System.out.println("Node '" + section + "' is not an element");
+            return null;
+        }
 
-	Element eElement = (Element) nNode;
+        Element eElement = (Element) nNode;
 
-	return eElement;
+        return eElement;
     }
 
     public static String getSectionValue(Element element, String section) {
-	Element sectionElement = getSection(element, section);
+        Element sectionElement = getSection(element, section);
 
-	return sectionElement.getTextContent();
+        return sectionElement.getTextContent();
     }
 
     public static Integer getAttributeInt(Document doc, String section, String attribute) {
-	String integerValue = getAttribute(doc, section, attribute);
+        String integerValue = getAttribute(doc, section, attribute);
 
-	return SpecsStrings.parseInteger(integerValue);
+        return SpecsStrings.parseInteger(integerValue);
     }
 
     public static Node getNode(NodeList nodes, String tag) {
-	return getNodeMaybe(nodes, tag)
-		.orElseThrow(
-			() -> new RuntimeException("Could not find a node with tag '" + tag + "' in nodes:\n" + nodes));
+        return getNodeMaybe(nodes, tag)
+                .orElseThrow(
+                        () -> new RuntimeException("Could not find a node with tag '" + tag + "' in nodes:\n" + nodes));
     }
 
     public static Optional<Node> getNodeMaybe(NodeList nodes, String tag) {
-	for (int i = 0; i < nodes.getLength(); i++) {
-	    Node currentNode = nodes.item(i);
-	    if (currentNode.getNodeName().equals(tag)) {
-		return Optional.of(currentNode);
-	    }
-	}
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Node currentNode = nodes.item(i);
+            if (currentNode.getNodeName().equals(tag)) {
+                return Optional.of(currentNode);
+            }
+        }
 
-	return Optional.empty();
+        return Optional.empty();
     }
 
     /*
     public static List<Node> getNodes(NodeList nodeList, String nodeTag) {
-
+    
     List<Node> nodes = new ArrayList<>();
     for (int i = 0; i < nodeList.getLength(); i++) {
         nodes.add(nodeList.item(i));
     }
-
+    
     return nodes;
     }
      */
 
     public static Optional<String> getAttribute(Node node, String attrName) {
-	if (node.getNodeType() == Node.ELEMENT_NODE) {
-	    return Optional.of(((Element) node).getAttribute(attrName));
-	}
+        if (node.getNodeType() == Node.ELEMENT_NODE) {
+            return Optional.of(((Element) node).getAttribute(attrName));
+        }
 
-	SpecsLogs.msgWarn("Given node that is not an element (" + node + "). Check if this works");
+        SpecsLogs.msgWarn("Given node that is not an element (" + node + "). Check if this works");
 
-	Node attribute = node.getAttributes().getNamedItem(attrName);
-	if (attribute == null) {
-	    return Optional.empty();
-	    // throw new RuntimeException("No attribute with name '"+attrName+"' in node '"+node+"');
-	}
+        Node attribute = node.getAttributes().getNamedItem(attrName);
+        if (attribute == null) {
+            return Optional.empty();
+            // throw new RuntimeException("No attribute with name '"+attrName+"' in node '"+node+"');
+        }
 
-	return Optional.of((attribute.getNodeValue()));
+        return Optional.of((attribute.getNodeValue()));
 
     }
 
     public static List<Node> getNodes(Node node, String tag) {
-	List<Node> children = new ArrayList<>();
+        List<Node> children = new ArrayList<>();
 
-	NodeList childrenList = node.getChildNodes();
-	for (int i = 0; i < childrenList.getLength(); i++) {
-	    Node child = childrenList.item(i);
-	    if (child.getNodeName().equals(tag)) {
-		children.add(child);
-	    }
-	}
+        NodeList childrenList = node.getChildNodes();
+        for (int i = 0; i < childrenList.getLength(); i++) {
+            Node child = childrenList.item(i);
+            if (child.getNodeName().equals(tag)) {
+                children.add(child);
+            }
+        }
 
-	return children;
+        return children;
     }
 
     /*
@@ -256,22 +256,40 @@ public class SpecsXml {
      */
 
     public static String getText(NodeList nodes, String... tagChain) {
-	NodeList currentNodes = nodes;
+        NodeList currentNodes = nodes;
 
-	for (int i = 0; i < tagChain.length; i++) {
-	    // Get node of current tag
-	    Node node = getNode(currentNodes, tagChain[i]);
+        for (int i = 0; i < tagChain.length; i++) {
+            // Get node of current tag
+            Node node = getNode(currentNodes, tagChain[i]);
 
-	    // If last node in the chain, return node value.
+            // If last node in the chain, return node value.
 
-	    if (i == tagChain.length - 1) {
-		return node.getTextContent();
-	    }
+            if (i == tagChain.length - 1) {
+                return node.getTextContent();
+            }
 
-	    // Otherwise, update current nodes
-	    currentNodes = node.getChildNodes();
-	}
+            // Otherwise, update current nodes
+            currentNodes = node.getChildNodes();
+        }
 
-	throw new RuntimeException("Should not arrive here.");
+        throw new RuntimeException("Should not arrive here.");
+    }
+
+    public static List<Element> getElementChildren(Element element) {
+        return getElementChildren(element, "*");
+    }
+
+    public static List<Element> getElementChildren(Element element, String tag) {
+        NodeList entries = element.getElementsByTagName(tag);
+        List<Element> children = new ArrayList<>();
+        for (int i = 0; i < entries.getLength(); i++) {
+            Node currentNode = entries.item(i);
+            if (currentNode instanceof Element) {
+                children.add((Element) currentNode);
+            }
+
+        }
+
+        return children;
     }
 }
