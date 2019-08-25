ce009537f05f900a16c0b3457636964ae5d5dd97
JoaoBispo <joaobispo@gmail.com>
2019-08-25 02:41:17 +0100

    [JsEngine] Adds launcher for tests

---
+++ JsEngine/src/pt/up/fe/specs/jsengine/JsEngineLauncher.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2019 SPeCS.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. under the License.
+ */
+
+package pt.up.fe.specs.jsengine;
+
+public class JsEngineLauncher {
+
+    public static void main(String[] args) {
+        var engine = JsEngineType.GRAALVM_COMPAT.newEngine();
+
+        System.out.println("Graal Compat: " + engine.eval("var a = 10; a;"));
+
+        var engine2 = JsEngineType.GRAALVM.newEngine();
+
+        System.out.println("Graal: " + engine2.eval("var a = 20; a;"));
+    }
+
+}
