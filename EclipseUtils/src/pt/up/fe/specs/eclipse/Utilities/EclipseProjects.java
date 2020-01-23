26c8f1fc7c8dddd404e05ec36078908e882991b8
JoaoBispo <joaobispo@gmail.com>
2020-01-23 17:05:00 +0000

    [EclipseUtils] Adds missing import

--- EclipseUtils/src/pt/up/fe/specs/eclipse/Utilities/EclipseProjects.java
+++ EclipseUtils/src/pt/up/fe/specs/eclipse/Utilities/EclipseProjects.java
@@ -24,9 +24,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.regex.Pattern;
 
-import org.w3c.dom.Attr;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 import pt.up.fe.specs.lang.SpecsPlatforms;
 import pt.up.fe.specs.util.SpecsFactory;
