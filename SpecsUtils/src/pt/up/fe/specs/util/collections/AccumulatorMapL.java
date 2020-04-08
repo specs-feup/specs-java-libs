10d1dcbaa2a039e9d071d508ffc6039738752bb4
João Bispo <joaobispo@gmail.com>
2020-04-08 11:54:14 +0100

    [SpecsUtils] Formatting changes

--- SpecsUtils/src/pt/up/fe/specs/util/collections/AccumulatorMapL.java
+++ SpecsUtils/src/pt/up/fe/specs/util/collections/AccumulatorMapL.java
@@ -27,15 +27,15 @@ import pt.up.fe.specs.util.SpecsLogs;
 public class AccumulatorMapL<T> {
 
     public AccumulatorMapL() {
-	this.accMap = new HashMap<>();
-	this.accumulator = 0;
-	this.unmodifiable = false;
+        this.accMap = new HashMap<>();
+        this.accumulator = 0;
+        this.unmodifiable = false;
     }
 
     public AccumulatorMapL(AccumulatorMapL<T> accumulatorMap) {
-	this.accMap = new HashMap<>(accumulatorMap.accMap);
-	this.accumulator = accumulatorMap.accumulator;
-	this.unmodifiable = false;
+        this.accMap = new HashMap<>(accumulatorMap.accMap);
+        this.accumulator = accumulatorMap.accumulator;
+        this.unmodifiable = false;
     }
 
     /**
@@ -44,12 +44,12 @@ public class AccumulatorMapL<T> {
      * @return
      */
     public AccumulatorMapL<T> getUnmodifiableMap() {
-	AccumulatorMapL<T> unmodMap = new AccumulatorMapL<>();
-	unmodMap.accMap = Collections.unmodifiableMap(this.accMap);
-	unmodMap.accumulator = this.accumulator;
-	unmodMap.unmodifiable = true;
+        AccumulatorMapL<T> unmodMap = new AccumulatorMapL<>();
+        unmodMap.accMap = Collections.unmodifiableMap(this.accMap);
+        unmodMap.accumulator = this.accumulator;
+        unmodMap.unmodifiable = true;
 
-	return unmodMap;
+        return unmodMap;
     }
 
     /**
@@ -59,7 +59,7 @@ public class AccumulatorMapL<T> {
      * @return the current number of added elements. If it is the first time we are adding an element, returns 1
      */
     public Long add(T element) {
-	return add(element, 1);
+        return add(element, 1);
     }
 
     /**
@@ -70,48 +70,48 @@ public class AccumulatorMapL<T> {
      * @return
      */
     public Long add(T element, long incrementValue) {
-	if (this.unmodifiable) {
-	    throw new UnsupportedOperationException("Attempting to modify an unmodifiable '" + AccumulatorMapL.class
-		    + "'.");
-	}
+        if (this.unmodifiable) {
+            throw new UnsupportedOperationException("Attempting to modify an unmodifiable '" + AccumulatorMapL.class
+                    + "'.");
+        }
 
-	Long value = this.accMap.get(element);
-	if (value == null) {
-	    value = 0l;
-	}
+        Long value = this.accMap.get(element);
+        if (value == null) {
+            value = 0l;
+        }
 
-	// int incrementValue = 1;
+        // int incrementValue = 1;
 
-	value += incrementValue;
-	this.accMap.put(element, value);
-	this.accumulator += incrementValue;
+        value += incrementValue;
+        this.accMap.put(element, value);
+        this.accumulator += incrementValue;
 
-	return value;
+        return value;
     }
 
     public boolean remove(T element) {
-	return remove(element, 1);
+        return remove(element, 1);
     }
 
     public boolean remove(T element, int incrementValue) {
-	if (this.unmodifiable) {
-	    throw new UnsupportedOperationException("Attempting to modify an unmodifiable '" + AccumulatorMap.class
-		    + "'.");
-	}
+        if (this.unmodifiable) {
+            throw new UnsupportedOperationException("Attempting to modify an unmodifiable '" + AccumulatorMap.class
+                    + "'.");
+        }
 
-	Long value = this.accMap.get(element);
-	if (value == null) {
-	    SpecsLogs.msgWarn("Map does not contain any instance of '" + element + "'");
-	    return false;
-	}
+        Long value = this.accMap.get(element);
+        if (value == null) {
+            SpecsLogs.msgWarn("Map does not contain any instance of '" + element + "'");
+            return false;
+        }
 
-	// int incrementValue = 1;
+        // int incrementValue = 1;
 
-	value -= incrementValue;
-	this.accMap.put(element, value);
-	this.accumulator -= incrementValue;
+        value -= incrementValue;
+        this.accMap.put(element, value);
+        this.accumulator -= incrementValue;
 
-	return true;
+        return true;
     }
 
     /**
@@ -120,13 +120,13 @@ public class AccumulatorMapL<T> {
      * @return the number of times the given element was added to the table.
      */
     public long getCount(T element) {
-	Long count = this.accMap.get(element);
-	if (count == null) {
-	    return 0l;
-	}
+        Long count = this.accMap.get(element);
+        if (count == null) {
+            return 0l;
+        }
 
-	// return accMap.get(element);
-	return count;
+        // return accMap.get(element);
+        return count;
     }
 
     /**
@@ -135,9 +135,9 @@ public class AccumulatorMapL<T> {
      * @return the ratio of the given element in relation to the other elements of the table.
      */
     public double getRatio(T element) {
-	Long count = getCount(element);
+        Long count = getCount(element);
 
-	return (double) count / (double) this.accumulator;
+        return (double) count / (double) this.accumulator;
     }
 
     /**
@@ -148,48 +148,48 @@ public class AccumulatorMapL<T> {
      */
     // public int getSum() {
     public long getSum() {
-	/*
-	int accumulator = 0;
-	for(T key : accMap.keySet()) {
-	   accumulator += accMap.get(key);
-	}
-
-	return accumulator;
-	 *
-	 */
-	return this.accumulator;
+        /*
+        int accumulator = 0;
+        for(T key : accMap.keySet()) {
+           accumulator += accMap.get(key);
+        }
+        
+        return accumulator;
+         *
+         */
+        return this.accumulator;
     }
 
     @Override
     public String toString() {
-	return this.accMap.toString();
+        return this.accMap.toString();
     }
 
     public Map<T, Long> getAccMap() {
-	return Collections.unmodifiableMap(this.accMap);
+        return Collections.unmodifiableMap(this.accMap);
     }
 
     @Override
     public boolean equals(Object obj) {
-	if (!(obj instanceof AccumulatorMapL)) {
-	    return false;
-	}
+        if (!(obj instanceof AccumulatorMapL)) {
+            return false;
+        }
 
-	AccumulatorMapL<?> anotherObj = ((AccumulatorMapL<?>) obj);
+        AccumulatorMapL<?> anotherObj = ((AccumulatorMapL<?>) obj);
 
-	if (this.accumulator != anotherObj.accumulator) {
-	    return false;
-	}
+        if (this.accumulator != anotherObj.accumulator) {
+            return false;
+        }
 
-	return this.accMap.equals(anotherObj.accMap);
+        return this.accMap.equals(anotherObj.accMap);
     }
 
     @Override
     public int hashCode() {
-	int hash = 7;
-	hash = 47 * hash + (this.accMap != null ? this.accMap.hashCode() : 0);
-	hash = 47 * hash + (int) (this.accumulator ^ (this.accumulator >>> 32));
-	return hash;
+        int hash = 7;
+        hash = 47 * hash + (this.accMap != null ? this.accMap.hashCode() : 0);
+        hash = 47 * hash + (int) (this.accumulator ^ (this.accumulator >>> 32));
+        return hash;
     }
 
     private Map<T, Long> accMap;
