37a92594634e1aae748cf74acad5cd67c4fb156b
João Bispo <joaobispo@gmail.com>
2020-05-22 17:57:37 +0100

    [SpecsUtils] Adapts FunctionClassMap to use new ClassMapper

--- SpecsUtils/src/pt/up/fe/specs/util/classmap/FunctionClassMap.java
+++ SpecsUtils/src/pt/up/fe/specs/util/classmap/FunctionClassMap.java
@@ -19,8 +19,8 @@ import java.util.Optional;
 import java.util.function.Function;
 
 import pt.up.fe.specs.util.Preconditions;
-import pt.up.fe.specs.util.SpecsLogs;
 import pt.up.fe.specs.util.exceptions.NotImplementedException;
+import pt.up.fe.specs.util.utilities.ClassMapper;
 
 /**
  * Maps a class T or subtype of T to a Function that accepts one argument T and produces a result R.
@@ -40,22 +40,24 @@ import pt.up.fe.specs.util.exceptions.NotImplementedException;
 public class FunctionClassMap<T, R> {
 
     private final Map<Class<? extends T>, Function<? extends T, R>> map;
-    private final boolean supportInterfaces;
+    // private final boolean supportInterfaces;
     // Can be null
     private final R defaultValue;
     // Can be null
     private final Function<T, ? extends R> defaultFunction;
 
+    private final ClassMapper classMapper;
+
     public FunctionClassMap() {
-        this(new HashMap<>(), true, null, null);
+        this(new HashMap<>(), null, null, new ClassMapper());
     }
 
     public <ER extends R> FunctionClassMap(ER defaultValue) {
-        this(new HashMap<>(), true, defaultValue, null);
+        this(new HashMap<>(), defaultValue, null, new ClassMapper());
     }
 
     public <ER extends R> FunctionClassMap(Function<T, ER> defaultFunction) {
-        this(new HashMap<>(), true, null, defaultFunction);
+        this(new HashMap<>(), null, defaultFunction, new ClassMapper());
     }
 
     @SuppressWarnings("unchecked")
@@ -68,22 +70,24 @@ public class FunctionClassMap<T, R> {
             this.map.put((Class<? extends T>) keyPair.getKey(), (Function<T, R>) keyPair.getValue());
         }
 
-        this.supportInterfaces = functionClassMap.supportInterfaces;
+        // this.supportInterfaces = functionClassMap.supportInterfaces;
         this.defaultValue = functionClassMap.defaultValue;
         this.defaultFunction = functionClassMap.defaultFunction;
+        this.classMapper = new ClassMapper(functionClassMap.classMapper);
     }
 
-    private <ER extends R> FunctionClassMap(Map<Class<? extends T>, Function<? extends T, R>> map,
-            boolean supportInterfaces,
-            R defaultValue, Function<T, ER> defaultFunction) {
+    private <ER extends R> FunctionClassMap(Map<Class<? extends T>, Function<? extends T, R>> map, R defaultValue,
+            Function<T, ER> defaultFunction, ClassMapper classMapper) {
 
         Preconditions.checkArgument(!(defaultFunction != null && defaultValue != null),
                 "Both defaults cannot be different than null at the same time");
 
         this.map = map;
-        this.supportInterfaces = supportInterfaces;
+        // this.supportInterfaces = supportInterfaces;
         this.defaultValue = defaultValue;
         this.defaultFunction = defaultFunction;
+
+        this.classMapper = classMapper;
     }
 
     /**
@@ -103,28 +107,47 @@ public class FunctionClassMap<T, R> {
     public <ET extends T, K extends ET> void put(Class<K> aClass,
             Function<ET, R> value) {
 
-        if (!this.supportInterfaces) {
-            if (aClass.isInterface()) {
-                SpecsLogs.msgWarn("Support for interfaces is disabled, map is unchanged");
-                return;
-            }
-        }
+        // if (!this.supportInterfaces) {
+        // if (aClass.isInterface()) {
+        // SpecsLogs.msgWarn("Support for interfaces is disabled, map is unchanged");
+        // return;
+        // }
+        // }
 
         this.map.put(aClass, value);
+        this.classMapper.add(aClass);
 
     }
 
     @SuppressWarnings("unchecked")
     private <TK extends T> Optional<Function<T, R>> get(Class<TK> key) {
-        Class<?> currentKey = key;
+        // Map given class to a class supported by this instance
+        var mappedClass = classMapper.map(key);
 
+        if (mappedClass.isEmpty()) {
+            return Optional.empty();
+        }
+
+        var function = this.map.get(mappedClass.get());
+        // var function = mappedClass.map(aClass -> this.map.get(aClass));
+
+        // if (function.isEmpty()) {
+        if (function == null) {
+            throw new RuntimeException("There should be a mapping for " + mappedClass.get() + ", verify");
+        }
+
+        return Optional.of((Function<T, R>) function);
+
+        /*
+        Class<?> currentKey = key;
+        
         while (currentKey != null) {
             // Test key
             Function<? extends T, R> result = this.map.get(currentKey);
             if (result != null) {
                 return Optional.of((Function<T, R>) result);
             }
-
+        
             if (this.supportInterfaces) {
                 for (Class<?> interf : currentKey.getInterfaces()) {
                     result = this.map.get(interf);
@@ -133,12 +156,12 @@ public class FunctionClassMap<T, R> {
                     }
                 }
             }
-
+        
             currentKey = currentKey.getSuperclass();
         }
-
+        
         return Optional.empty();
-
+        */
     }
 
     @SuppressWarnings("unchecked")
@@ -216,11 +239,11 @@ public class FunctionClassMap<T, R> {
      * @return
      */
     public FunctionClassMap<T, R> setDefaultValue(R defaultValue) {
-        return new FunctionClassMap<>(this.map, this.supportInterfaces, defaultValue, null);
+        return new FunctionClassMap<>(this.map, defaultValue, null, this.classMapper);
     }
 
     public <ER extends R> FunctionClassMap<T, R> setDefaultFunction(Function<T, ER> defaultFunction) {
-        return new FunctionClassMap<>(this.map, this.supportInterfaces, null, defaultFunction);
+        return new FunctionClassMap<>(this.map, null, defaultFunction, this.classMapper);
     }
 
 }
