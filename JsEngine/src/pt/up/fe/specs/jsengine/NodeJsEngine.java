d82436df028c80723946e060a57fb3fa9ac499eb
João Bispo <jbispo@fe.up.pt>
2024-07-25 21:52:59 +0100

    [JsEngine] Makes NodeJsEngine undefined a singleton

--- JsEngine/src/pt/up/fe/specs/jsengine/NodeJsEngine.java
+++ JsEngine/src/pt/up/fe/specs/jsengine/NodeJsEngine.java
@@ -1,31 +1,31 @@
 /**
- *  Copyright 2023 SPeCS.
- * 
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- * 
- *       http://www.apache.org/licenses/LICENSE-2.0
- * 
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *  under the License.
+ * Copyright 2023 SPeCS.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * under the License.
  */
 
 package pt.up.fe.specs.jsengine;
+
+import pt.up.fe.specs.jsengine.node.UndefinedValue;
+import pt.up.fe.specs.util.classmap.FunctionClassMap;
+import pt.up.fe.specs.util.exceptions.NotImplementedException;
 
 import java.io.File;
 import java.util.Collection;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
-
-import pt.up.fe.specs.jsengine.node.UndefinedValue;
-import pt.up.fe.specs.util.classmap.FunctionClassMap;
-import pt.up.fe.specs.util.exceptions.NotImplementedException;
 
 public class NodeJsEngine implements JsEngine {
 
@@ -127,7 +127,7 @@ public class NodeJsEngine implements JsEngine {
 
     @Override
     public Object getUndefined() {
-        return new UndefinedValue();
+        return UndefinedValue.getUndefined();
     }
 
     @Override
--- JsEngine/src/pt/up/fe/specs/jsengine/node/UndefinedValue.java
+++ JsEngine/src/pt/up/fe/specs/jsengine/node/UndefinedValue.java
@@ -1,18 +1,18 @@
 /**
- *  Copyright 2023 SPeCS.
- * 
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- * 
- *       http://www.apache.org/licenses/LICENSE-2.0
- * 
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *  under the License.
+ * Copyright 2023 SPeCS.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * under the License.
  */
 
 package pt.up.fe.specs.jsengine.node;
@@ -21,5 +21,10 @@ package pt.up.fe.specs.jsengine.node;
  * Dummy class to represent javascript's "undefined" in Java. Used by NodeJsEngine as a return value.
  */
 public class UndefinedValue {
-    
+
+    private static final UndefinedValue UNDEFINED = new UndefinedValue();
+
+    public static UndefinedValue getUndefined() {
+        return UNDEFINED;
+    }
 }
