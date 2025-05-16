802110ff296dbdc6190cd84714a98b335e7d41be
João Bispo <jbispo@fe.up.pt>
2025-05-16 19:06:42 +0100

    Adds JsonReaderParser.nextValue(), which generically parses the next value

--- GsonPlus/src/org/suikasoft/GsonPlus/JsonReaderParser.java
+++ GsonPlus/src/org/suikasoft/GsonPlus/JsonReaderParser.java
@@ -5,10 +5,34 @@ import pt.up.fe.specs.util.SpecsCheck;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 
 public interface JsonReaderParser {
+
+    default Object nextValue(JsonReader reader, String name) {
+        nextName(reader, name);
+        return nextValue(reader);
+    }
+
+    default Object nextValue(JsonReader reader) {
+        try {
+            var next = reader.peek();
+            return switch (next) {
+                case STRING -> reader.nextString();
+                case BOOLEAN -> reader.nextBoolean();
+                case NUMBER -> reader.nextDouble();
+                case BEGIN_ARRAY -> nextList(reader);
+                case BEGIN_OBJECT -> nextObject(reader);
+                default -> throw new RuntimeException("Case not defined: " + next);
+            };
+
+        } catch (IOException e) {
+            throw new RuntimeException("Could not read value from JSON", e);
+        }
+    }
 
     default String nextName(JsonReader reader, String name) {
         try {
@@ -30,6 +54,46 @@ public interface JsonReaderParser {
             return reader.nextString();
         } catch (IOException e) {
             throw new RuntimeException("Could not read string from JSON", e);
+        }
+    }
+
+    default List<Object> nextList(JsonReader reader) {
+        try {
+            var list = new ArrayList<>();
+
+
+            reader.beginArray();
+
+            while (reader.hasNext()) {
+                list.add(nextValue(reader));
+            }
+
+            reader.endArray();
+
+            return list;
+        } catch (IOException e) {
+            throw new RuntimeException("Could not read list from JSON", e);
+        }
+    }
+
+    default Map<String, Object> nextObject(JsonReader reader) {
+        try {
+            var map = new HashMap<String, Object>();
+
+
+            reader.beginObject();
+
+            while (reader.hasNext()) {
+                var key = reader.nextName();
+                var value = nextValue(reader);
+                map.put(key, value);
+            }
+
+            reader.endObject();
+
+            return map;
+        } catch (IOException e) {
+            throw new RuntimeException("Could not read list from JSON", e);
         }
     }
 
@@ -55,6 +119,29 @@ public interface JsonReaderParser {
             return list;
         } catch (IOException e) {
             throw new RuntimeException("Could not read list from JSON", e);
+        }
+    }
+
+    /**
+     * Assumes we are inside an object, and there are only key-value strings until the end
+     *
+     * @param reader
+     * @return
+     */
+    default public Map<String, String> fillMap(JsonReader reader) {
+        try {
+            var attrs = new HashMap<String, String>();
+            while (reader.hasNext()) {
+
+                var key = reader.nextName();
+                var value = nextString(reader);
+
+                attrs.put(key, value);
+            }
+
+            return attrs;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
