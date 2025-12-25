fb2083401bf7d0c57ed98442fbc9112e34b1133c
L. Sousa <lm.sousa@fe.up.pt>
2025-12-25 23:43:00 +0000

    Remove unused methods and imports from PostProcessUtils for cleaner code

--- EclipseUtils/src/pt/up/fe/specs/eclipse/Utilities/PostProcessUtils.java
+++ EclipseUtils/src/pt/up/fe/specs/eclipse/Utilities/PostProcessUtils.java
@@ -13,22 +13,7 @@
 
 package pt.up.fe.specs.eclipse.Utilities;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.nio.file.FileVisitResult;
-import java.nio.file.FileVisitor;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -64,15 +49,6 @@ public class PostProcessUtils {
         default:
             throw new RuntimeException("Extension not supported: " + extension);
         }
-    }
-
-    /**
-     * Based from here: https://stackoverflow.com/questions/11502260/modifying-a-text-file-in-a-zip-archive-in-java
-     * 
-     * @param jarFile
-     */
-    private static void processJarFile(File jarFile) {
-        processFile(Paths.get(jarFile.getAbsolutePath()), MANIFEST_PATH);
     }
 
     private static void processJarFileExtract(File jarFile) {
@@ -119,115 +95,5 @@ public class PostProcessUtils {
 
         // SpecsIo.zip(null, zipFile, zipFile);
         SpecsIo.deleteFolder(tempFolder);
-    }
-
-    private static void processZipFileInplace(File zipFile) {
-        // Find JAR file
-        Path zipFilePath = Paths.get(zipFile.getAbsolutePath());
-
-        try (FileSystem fs = FileSystems.newFileSystem(zipFilePath, (ClassLoader) null)) {
-
-            // for (var rootPath : fs.getRootDirectories()) {
-            // System.out.println("OATH: " + rootPath);
-            // }
-            var jarFiles = new ArrayList<Path>();
-
-            Files.walkFileTree(fs.getRootDirectories().iterator().next(), Collections.emptySet(), 1,
-                    new FileVisitor<Path>() {
-                        @Override
-                        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
-                                throws IOException {
-                            // System.out.println("preVisitDirectory: " + dir);
-                            return FileVisitResult.CONTINUE;
-                        }
-
-                        @Override
-                        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
-                                throws IOException {
-                            // System.out.println("visitFile: " + file);
-
-                            var ext = SpecsIo.getExtension(file.toString()).toLowerCase();
-                            if ("jar".equals(ext)) {
-                                jarFiles.add(file);
-                            }
-
-                            return FileVisitResult.CONTINUE;
-                        }
-
-                        @Override
-                        public FileVisitResult visitFileFailed(Path file, IOException exc)
-                                throws IOException {
-                            // System.out.println("visitFileFailed: " + file);
-                            return FileVisitResult.CONTINUE;
-                        }
-
-                        @Override
-                        public FileVisitResult postVisitDirectory(Path dir, IOException exc)
-                                throws IOException {
-                            // System.out.println("postVisitDirectory: " + dir);
-                            return FileVisitResult.CONTINUE;
-                        }
-                    });
-
-            System.out.println("JAR FILES: " + jarFiles);
-
-            var jarPath = jarFiles.get(0);
-
-            processFile(jarPath, MANIFEST_PATH);
-
-        } catch (IOException e) {
-            throw new RuntimeException("Could not process zip file '" + zipFile.getAbsolutePath() + "'", e);
-        }
-
-        // processFile(jarFile, MANIFEST_PATH);
-    }
-
-    private static void processFile(Path zipFilePath, String manifestPath) {
-
-        try (FileSystem fs = FileSystems.newFileSystem(zipFilePath, (ClassLoader) null)) {
-            Path source = fs.getPath(manifestPath);
-
-            SpecsCheck.checkArgument(Files.exists(source),
-                    () -> "Could not find path '" + manifestPath + "' inside path '" + zipFilePath + "'");
-
-            var tempName = manifestPath + ".eclipse-utils-temp";
-            Path temp = fs.getPath(tempName);
-            if (Files.exists(temp)) {
-                SpecsLogs.info("Deleting temporary file '" + tempName + "' inside '" + zipFilePath + "'");
-                Files.delete(temp);
-            }
-            Files.move(source, temp);
-            modifyManifest(temp, source);
-            Files.delete(temp);
-        } catch (IOException e) {
-            throw new RuntimeException("Could not process JAR file", e);
-        }
-
-    }
-
-    /**
-     * Based from here: https://stackoverflow.com/questions/11502260/modifying-a-text-file-in-a-zip-archive-in-java
-     * 
-     * @param src
-     * @param dst
-     * @throws IOException
-     */
-    static void modifyManifest(Path src, Path dst) throws IOException {
-        try (BufferedReader br = new BufferedReader(
-                new InputStreamReader(Files.newInputStream(src)));
-                BufferedWriter bw = new BufferedWriter(
-                        new OutputStreamWriter(Files.newOutputStream(dst)))) {
-
-            // Simply replace whatever new line is being used with just \n
-
-            String line;
-            while ((line = br.readLine()) != null) {
-                // line = line.replace("key1=value1", "key1=value2");
-                bw.write(line);
-
-                bw.write("\n");
-                // bw.newLine();
-            }
-        }
     }
 }
